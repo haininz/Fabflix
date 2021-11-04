e10823acf72dce5463bb34b0d7d70e239c7c4984
Hertz <hertz.yang@zongmutech.com>
2021-11-04 13:29:10 -0700

    add star

---
+++ src/AddStar.java
@@ -0,0 +1,9 @@
+public class AddStar {
+
+    public static void main(String[] args) {
+        SAXParser temp = new SAXParser();
+        temp.runExample();
+        System.out.println(temp.getMoviesList().toString());
+
+    }
+}
